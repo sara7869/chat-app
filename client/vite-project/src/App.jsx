cd8c4c5ec98ed5c1017d17aab5f4a3df65de9cf0
sara7869 <sarayusri17@gmail.com>
2024-03-19 12:11:28 +0530

    Created UI

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import React from 'react';
-import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
 
 import socketIO from "socket.io-client";
 const socket = socketIO.connect("http://localhost:4000");
@@ -9,12 +10,14 @@ const socket = socketIO.connect("http://localhost:4000");
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <p>Hello World!</p>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
       </div>
-
-    </>
+    </BrowserRouter>
   );
 }
 
